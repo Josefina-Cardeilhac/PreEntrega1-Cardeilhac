01bbc15a1bc38532e997ce2f92372b26aef5157d
Josefina Cardeilhac <cardeilhac.josefina@gmail.com>
2023-09-21 00:55:40 -0300

    agregandocard

--- preentrega1/src/App.jsx
+++ preentrega1/src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
 import Header from './components/Header/Header'
+import CardUser from './components/Card user/CardUser'
 
 const App = () => {
   return (
     <div>
     <Header />
+    <div className='userecction'>
+      <CardUser />
     </div>
+    </div>
+    
   )
 }
 export default App

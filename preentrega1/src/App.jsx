4a32d9e8579aff5b9a1493df089d236edd20070b
Josefina Cardeilhac <cardeilhac.josefina@gmail.com>
2023-09-21 02:11:53 -0300

    agrgegandoC

--- preentrega1/src/App.jsx
+++ preentrega1/src/App.jsx
@@ -1,13 +1,18 @@
 import './App.css'
 import Header from './components/Header/Header'
-import CardUser from './components/Card user/CardUser'
+import CardUser from './components/CardUser/CardUser'
 
 const App = () => {
   return (
     <div>
     <Header />
-    <div className='userecction'>
-      <CardUser />
+    <div className='UserSection'>
+      <CardUser 
+      name="Josefina Cardeilhac"
+      date ="Se unio en Marzo 2012"
+      description= "nkjfnklfnlk"
+      img="http://react.semantic-ui.com/images/avatar/large/elliot.jpg"/>
+      
     </div>
     </div>
     
